3927fa641a131acf374622758c5257c470944acf
kahveciderin <kahveciderin@gmail.com>
2020-09-22 08:51:09 +0300

    change

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 
 
 int main(int argc, char* argv[]){
-    /* initialize random seed: */
-    
+
     srand (time(NULL));
     string data;
     virtualmachine machinestate;
@@ -18,15 +17,10 @@ int main(int argc, char* argv[]){
         data += "\n";
     }
 
-    //printf("%s", data.c_str());
-
-    //uint16_t* a = compile("NOP#NN 0000 0000\n:str\nNOP#NN 0000 0000\nJMP#NN 0005 .str\n");
 
     uint16_t* a = compile(data + "\n");
 
-    //for(int x = 0; x < 65535; x++){
-    //    printf("0x%x\n", a[x]);
-    //}
+
     machinestate.push = false;
     machinestate.halt = false;
     machinestate.addrspace = a;
